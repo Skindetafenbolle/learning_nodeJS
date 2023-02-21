1825a8179456adc1f2f02f8dbf4d9592fb748c1e
Kulbatsky <elited013@gmail.cpm>
2023-02-21 14:36:50 +0300

    lesson 4/ work with Modules

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // import os from 'os'
 // console.log(os.platform());
 
+//lesson 4 - modules
+import { add,minus } from './math.js'
+let res1 = add(4,5)
+let res2 = minus(6,2)
+console.log(res1,res2)
+
