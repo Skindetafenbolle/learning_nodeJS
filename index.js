e38b5b77d41189fe935b57f9efbfd20d72ec3626
Kulbatsky <elited013@gmail.cpm>
2023-02-21 14:57:40 +0300

    lesson 5/ work with files

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
 // console.log(os.platform());
 
 //lesson 4 - modules
-import { add,minus } from './math.js'
-let res1 = add(4,5)
-let res2 = minus(6,2)
-console.log(res1,res2)
+// import { add,minus } from './math.js'
+// let res1 = add(4,5)
+// let res2 = minus(6,2)
+// console.log(res1,res2)
 
+//lesson 5 - files
+import fs from 'fs'
+fs.readFile('some.sh','utf-8', (err,data)=>{
+    fs.writeFile('some.sh',data + 'Hello World from NodeJS', (err,data) =>{
+        console.log('congrats');
+    })
+})
