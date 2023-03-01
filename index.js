6c082c212be82fdff1442cb607972cbbf66752c9
Kulbatsky <elited013@gmail.cpm>
2023-03-01 08:38:05 +0300

    lesson 9/ Express JS

--- index.js
+++ index.js
@@ -1,59 +1,22 @@
-// import os from 'os'
-// console.log(os.platform());
+import express from 'express'
 
-//lesson 4 - modules
-// import { add,minus } from './math.js'
-// let res1 = add(4,5)
-// let res2 = minus(6,2)
-// console.log(res1,res2)
-
-//lesson 5 - files
-// import fs from 'fs'
-// fs.readFile('some.sh','utf-8', (err,data)=>{
-//     fs.writeFile('some.sh',data + 'Hello World from NodeJS', (err,data) =>{
-//         console.log('congrats');
-//     })
-// })
-
-//lesson 6 - directory's
-// import fs from 'fs'
-// fs.mkdir('text-files', ()=>{
-//     fs.writeFile('./text-files/some.sh','Hello', ()=>{
-//
-//     })
-// })
-//
-// fs.unlink('./text-files/some.sh', ()=>{
-//     fs.rmdir('./text-files', ()=>{})
-// })
-
-// lesson 7 - created server
-import http from "http"
-import fs from 'fs'
-
+const app = express()
 const PORT = 3000
-const HOST = 'localhost'
-let server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
 
-    if(req.url === '/'){
-        fs.createReadStream(
-            './templates/index.html'
-        ).pipe(res)
-    }
-    else if (req.url === '/about')
-        fs.createReadStream(
-            './templates/about.html'
-        ).pipe(res)
-    else
-        fs.createReadStream(
-            './templates/error.html'
-        ).pipe(res)
-
-    // const stream = fs.createReadStream('./templates/index.html')
-    // stream.pipe(res)
-})
-server.listen(PORT, HOST, () => {
-    console.log(`Connected to Server`)
+app.get('/', (req, res)=>{
+    res.status(200)
+    res.send('Home page')
 })
 
+app.get('/about', (req, res)=>{
+    res.status(200)
+    res.send('About page')
+})
+
+app.get('/user/:userName/:id', (req,res)=>{
+    res.send(`User id ${req.params.id}`)
+})
+
+app.listen(PORT, ()=>{
+    console.log('Server started');
+})
